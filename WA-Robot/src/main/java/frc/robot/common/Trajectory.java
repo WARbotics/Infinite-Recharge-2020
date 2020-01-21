1a340157d662e4284cf9b521a6ffb7b20a04b85f
Victor Henriksson <vhenrixon@gmail.com>
2020-01-21 17:47:57 -0500

    description for trajectroy object

--- WA-Robot/src/main/java/frc/robot/common/Trajectory.java
+++ WA-Robot/src/main/java/frc/robot/common/Trajectory.java
@@ -4,12 +4,19 @@ package frc.robot.common;
 
 
 public class Trajectory{ 
+    /*
+        This object calculates the trajectory for a fixed angle shooter.
+        Note: The equation becomes imagengery if the kX < [13.271, 0] and the kY and kX are in meter 
 
-    private double kTheta = 37.0;
+        Contributed by: Victor Henriksson
+    */
+    private double kTheta = 37.0; // Degree
     private double kY = 0;// Height shooting board
     private double kG = -9.8; // Acceleration due to gravity
-    public Trajectory(double kY){
+    private double correctionFactor = 0;
+    public Trajectory(double kY, double correctionFactor){
         this.kY = kY;
+        this.correctionFactor = correctionFactor;
     }
 
     public void setTheta(double theta){
@@ -23,7 +30,7 @@ public class Trajectory{
             double sinTheta = Math.sin(Math.toRadians(kTheta));
             double num = .5*(kG)*kX*kX;
             double denum = cosTheta*(kY*cosTheta-sinTheta*kX);
-            return Math.sqrt(num/denum);
+            return Math.sqrt(num/denum) + correctionFactor; // Returns m/s
         }catch(ArithmeticException e){
             throw e; // If the equation was imagernary 
         }
