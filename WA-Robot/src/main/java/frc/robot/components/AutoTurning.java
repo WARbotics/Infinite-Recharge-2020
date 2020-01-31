057d862b35233dd35e803a4af5c47a36e7d694a7
Bruce Tan <21btan@woodward.edu>
2020-01-31 18:00:17 -0500

    GO WAR

--- WA-Robot/src/main/java/frc/robot/components/AutoTurning.java
+++ WA-Robot/src/main/java/frc/robot/components/AutoTurning.java
@@ -6,7 +6,7 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class AutoTurn {
+public class AutoTurn extends AutoCommand{
     /*
     *This object receives all the motors for drivetrain and initiates NavX Micro (for angle) and drivetrain(four motors)
     *For the rotating method, it receives the desired angle and the desired speed
@@ -19,13 +19,14 @@ public class AutoTurn {
     static private Drivetrain robotDrive;
 
     public AutoTurn(WPI_TalonSRX leftLeadTalonSRX, WPI_VictorSPX leftFollowSPX, WPI_TalonSRX rightLeadSRX,
-    WPI_TalonSRX rightFollowSPX) {
+    WPI_TalonSRX rightFollowSPX, String name, double time) {
+        super(name, time);
         ahrsDevice = new AHRS(SPI.Port.kMXP);
         robotDrive = new Drivetrain(leftLeadTalonSRX, leftFollowSPX, rightLeadSRX,
         rightFollowSPX);
     }
 
-    public void rotating (double speed, double angle) {
+    public void robotRotating (double speed, double angle) {
         double angleDiff = angle - ahrsDevice.getAngle();
         double rotation = angleDiff * 0.05;
     
