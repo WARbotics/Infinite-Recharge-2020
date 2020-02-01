92fba7da2da03b111048808c630a2693afcd0c1b
Lily P <lilyp@Lilys-MacBook-Pro.local>
2020-01-31 22:01:44 -0500

    Fixed errors

--- WA-Robot/src/main/java/frc/robot/common/AutoCommands/AutoMove.java
+++ WA-Robot/src/main/java/frc/robot/common/AutoCommands/AutoMove.java
@@ -9,12 +9,11 @@ public class AutoMove extends AutoCommand{
 
     private Drivetrain drive;
     public double speed = 0.0;
-    public double time = 0.0;
-    public double dist = 0.0;
     public double setPoint = 0.0;
     private PID PID = new PID(0.1, 0.00, 0.00);
     
     public AutoMove(Drivetrain drive, double speed, double time, double setPoint){
+        super("AutoMove", time);
         this.drive = drive;
         this.setPoint = setPoint;
         
@@ -24,17 +23,12 @@ public class AutoMove extends AutoCommand{
         this.speed = speed;
     }
 
-    public void distDiff(double dist){
-        this.dist = dist;
-    }
 
     public void setDistance(double setPoint){
         this.setPoint = setPoint;
     }
 
-    public void setTime(double time){
-        this.time = time;
-    }
+   
     
 
     
