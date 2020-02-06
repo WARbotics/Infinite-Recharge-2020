96359d1045b0fca4c2d062f446fdcd61c30d67dc
Victor Henriksson <vhenrixon@gmail.com>
2020-02-06 10:20:59 -0500

    minor change

--- WA-Robot/src/main/java/frc/robot/components/Conveyor.java
+++ WA-Robot/src/main/java/frc/robot/components/Conveyor.java
@@ -19,14 +19,15 @@ public class Conveyor{
     private ConveyorMode mode = ConveyorMode.OFF;
     
 
-    private final WPI_TalonSRX frontConveyor;
-    private final WPI_TalonSRX backConveyor;
+    private final WPI_TalonSRX frontConveyorMotor;
+    private final WPI_TalonSRX backConveyorMotor;
     private TimeOfFlight ballSensor;
-    double threshold = .25;
-    public Conveyor(WPI_TalonSRX frontConveyor, WPI_TalonSRX backConveyor, TimeOfFlight ballSensor){
-    this.frontConveyor= frontConveyor;
-    this.backConveyor= backConveyor;
-    this.ballSensor = ballSensor;
+    private double threshold = .25;
+    public Conveyor(WPI_TalonSRX frontConveyorMotor, WPI_TalonSRX backConveyorMotor, TimeOfFlight ballSensor, double threshold){
+        this.frontConveyorMotor= frontConveyorMotor;
+        this.backConveyorMotor= backConveyorMotor;
+        this.ballSensor = ballSensor;
+        this.threshold = threshold;
      } 
     
     DoubleSolenoid hardStop = new DoubleSolenoid(1, 2);
@@ -45,21 +46,21 @@ public class Conveyor{
     }
 
     public void on(){
-        frontConveyor.set(1);
-        backConveyor.set(1);
+        frontConveyorMotor.set(1);
+        backConveyorMotor.set(1);
         mode = ConveyorMode.FORWARDS;
     
 
     }
     public void off(){
-        frontConveyor.set(0);
-        backConveyor.set(0);
+        frontConveyorMotor.set(0);
+        backConveyorMotor.set(0);
         mode = ConveyorMode.OFF;
     }
 
     public void backwards(){
-        frontConveyor.set(-1);
-        backConveyor.set(-1);
+        frontConveyorMotor.set(-1);
+        backConveyorMotor.set(-1);
         mode = ConveyorMode.BACKWARDS;
     
     }
